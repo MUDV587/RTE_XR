72b5525b4dce616e81de09f1ed75f0f954cd2572
Battlehub0x <Vadim.Andriyanov@outlook.com>
2020-03-17 20:19:10 +0200

    Selection outline

---
+++ RTE_XR/Assets/RTHandlesURP/Scripts/RenderMeshesCache.cs
@@ -0,0 +1,157 @@
+﻿using Battlehub.RTCommon;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battlehub.RTHandles.URP
+{
+    public class RenderMeshesBatch
+    {
+        public readonly Mesh Mesh;
+        public readonly List<Transform> Transforms = new List<Transform>();
+
+        private Matrix4x4[] m_matrices = new Matrix4x4[0];
+        public Matrix4x4[] Matrices
+        {
+            get { return m_matrices; }
+        }
+
+        public RenderMeshesBatch(Mesh mesh)
+        {
+            Mesh = mesh;
+        }
+
+        public void Refresh()
+        {
+            for (int i = Transforms.Count - 1; i >= 0; --i)
+            {
+                if (Transforms[i] == null)
+                {
+                    Transforms.RemoveAt(i);
+                }
+            }
+
+            if(Transforms.Count != m_matrices.Length)
+            {
+                m_matrices = new Matrix4x4[Transforms.Count];
+            }
+
+            for (int i = Transforms.Count - 1; i >= 0; --i)
+            {
+                m_matrices[i] = Transforms[i].localToWorldMatrix;
+            }
+        }
+    }
+
+    public interface IRenderMeshesCache
+    {
+        Material Material
+        {
+            get;
+        }
+
+        RenderMeshesBatch[] Batches
+        {
+            get;
+        }
+        void Add(Mesh mesh, Transform transform);
+        void Remove(Mesh mesh, Transform transform);
+        void Refresh(int maxBatchSize = 128);
+        void Clear();
+    }
+
+    public class RenderMeshesCache : MonoBehaviour, IRenderMeshesCache
+    {
+        private RenderMeshesBatch[] m_batches = new RenderMeshesBatch[0];
+        public RenderMeshesBatch[] Batches
+        {
+            get { return m_batches; }
+        }
+
+        [SerializeField]
+        private Material m_material = null;
+        public Material Material
+        {
+            get { return m_material; }
+        }
+
+        private Dictionary<Mesh, List<Transform>> m_meshToTransform = new Dictionary<Mesh, List<Transform>>();
+        private void Awake()
+        {
+            IOC.RegisterFallback<IRenderMeshesCache>(this);
+        }
+
+        private void OnDestroy()
+        {
+            IOC.UnregisterFallback<IRenderMeshesCache>(this);
+        }
+
+        private void Update()
+        {
+            for(int i = 0; i < m_batches.Length; ++i)
+            {
+                m_batches[i].Refresh();
+            }
+        }
+
+        public void Add(Mesh mesh, Transform transform)
+        {
+            List<Transform> transforms;
+            if (!m_meshToTransform.TryGetValue(mesh, out transforms))
+            {
+                transforms = new List<Transform>();
+                m_meshToTransform.Add(mesh, transforms);
+            }
+            transforms.Add(transform);
+        }
+
+        public void Remove(Mesh mesh, Transform transform)
+        {
+            List<Transform> transforms;
+            if (m_meshToTransform.TryGetValue(mesh, out transforms))
+            {
+                transforms.Remove(transform);
+                if (transforms.Count == 0)
+                {
+                    m_meshToTransform.Remove(mesh);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            m_meshToTransform.Clear();
+        }
+
+        public void Refresh(int maxBatchSize = 128)
+        {
+            List<RenderMeshesBatch> batches = new List<RenderMeshesBatch>();
+            foreach (KeyValuePair<Mesh, List<Transform>> kvp in m_meshToTransform)
+            {
+                if(kvp.Key == null)
+                {
+                    continue;
+                }
+
+                RenderMeshesBatch batch = new RenderMeshesBatch(kvp.Key);
+                batches.Add(batch);
+
+                List<Transform> transforms = kvp.Value;
+                int index = 0;
+                for (int i = 0; i < transforms.Count; ++i)
+                {
+                    if (index == maxBatchSize)
+                    {
+                        batch = new RenderMeshesBatch(kvp.Key);
+                        batches.Add(batch);
+                        index = 0;
+                    }
+
+                    batch.Transforms.Add(transforms[i]);
+                    index++;
+                }
+            }
+
+            m_batches = batches.ToArray();
+        }
+    }
+}
---
+++ RTE_XR/Assets/RTHandlesURP/Scripts/RenderSelection.cs
@@ -0,0 +1,167 @@
+﻿using Battlehub.RTCommon;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Battlehub.RTHandles.URP
+{
+   
+    public class RenderSelection : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class RenderSelectionSettings
+        {
+            public RenderPassEvent RenderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+            public Material PrepassMaterial = null;
+            public Material BlurMaterial = null;
+            public Material CompositeMaterial = null;
+            public Color OutlineColor = new Color32(255, 128, 0, 255);
+
+            [Range(0.5f, 10f)]
+            public float OutlineStength = 5;
+
+            [Range(0.1f, 3)]
+            public float BlurSize = 1f;
+        }
+
+        [SerializeField]
+        public RenderSelectionSettings m_settings = new RenderSelectionSettings();
+
+        class RenderSelectionPass : ScriptableRenderPass
+        {
+            public RenderSelectionSettings Settings;
+
+            private IRenderMeshesCache m_cache;
+                        
+            private int m_prepassId;
+            private RenderTargetIdentifier m_prepassRT;
+
+            private int m_blurredId;
+            private RenderTargetIdentifier m_blurredRT;
+
+            private int m_tmpTexId;
+            private RenderTargetIdentifier m_tmpRT;
+
+            private RenderTargetIdentifier m_cameraColorRT;
+
+            private int m_outlineColorId;
+            private int m_outlineStrengthId;
+            private int m_blurDirectionId;
+
+            public void Setup(RenderTargetIdentifier camerColorRT)
+            {
+                m_cache = IOC.Resolve<IRenderMeshesCache>();
+                m_cameraColorRT = camerColorRT;
+            }
+
+            private RenderTextureDescriptor GetStereoCompatibleDescriptor(RenderTextureDescriptor descriptor, int width, int height, GraphicsFormat format, int depthBufferBits = 0)
+            {
+                // Inherit the VR setup from the camera descriptor
+                var desc = descriptor;
+                desc.depthBufferBits = depthBufferBits;
+                desc.msaaSamples = 1;
+                desc.width = width;
+                desc.height = height;
+                desc.graphicsFormat = format;
+                return desc;
+            }
+
+            public override void Configure(CommandBuffer cmd, RenderTextureDescriptor camDesc)
+            {
+                if(m_cache == null)
+                {
+                    return;
+                }
+                
+                var width = camDesc.width;
+                var height = camDesc.height;
+
+                m_prepassId = Shader.PropertyToID("_PrepassTex");
+                m_blurredId = Shader.PropertyToID("_BlurredTex");
+                m_tmpTexId = Shader.PropertyToID("_TmpTex");
+                m_outlineColorId = Shader.PropertyToID("_OutlineColor");
+                m_outlineStrengthId = Shader.PropertyToID("_OutlineStrength");
+                m_blurDirectionId = Shader.PropertyToID("_BlurDirection");
+
+                var desc = GetStereoCompatibleDescriptor(camDesc, width, height, camDesc.graphicsFormat);
+                cmd.GetTemporaryRT(m_prepassId, desc);
+                cmd.GetTemporaryRT(m_blurredId, desc);
+                cmd.GetTemporaryRT(m_tmpTexId, desc);
+
+                m_prepassRT = new RenderTargetIdentifier(m_prepassId);
+                m_blurredRT = new RenderTargetIdentifier(m_blurredId);
+                m_tmpRT = new RenderTargetIdentifier(m_tmpTexId);
+
+                ConfigureTarget(m_prepassRT);
+                ConfigureClear(ClearFlag.Color, new Color(0, 0, 0, 1));
+            }
+
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                if (m_cache == null)
+                {
+                    return;
+                }
+
+                CommandBuffer cmd = CommandBufferPool.Get();
+               
+                RenderMeshesBatch[] batches = m_cache.Batches;
+                for (int i = 0; i < batches.Length; ++i)
+                {
+                    RenderMeshesBatch batch = batches[i];
+                    for (int j = 0; j < batch.Mesh.subMeshCount; ++j)
+                    {
+                        cmd.DrawMeshInstanced(batch.Mesh, j, Settings.PrepassMaterial, 0, batch.Matrices, batch.Matrices.Length);
+                    }
+                }
+
+                cmd.Blit(m_prepassRT, m_blurredRT);
+                cmd.SetGlobalFloat(m_outlineStrengthId, Settings.OutlineStength);
+                cmd.SetGlobalVector(m_blurDirectionId, new Vector2(Settings.BlurSize, 0));
+                cmd.Blit(m_blurredRT, m_tmpRT, Settings.BlurMaterial, 0);
+                cmd.SetGlobalVector(m_blurDirectionId, new Vector2(0, Settings.BlurSize));
+                cmd.Blit(m_tmpRT, m_blurredRT, Settings.BlurMaterial, 0);
+                
+                cmd.Blit(m_cameraColorRT, m_tmpRT);
+                cmd.SetGlobalTexture(m_prepassId, m_prepassRT);
+                cmd.SetGlobalTexture(m_blurredId, m_blurredId);
+                cmd.SetGlobalColor(m_outlineColorId, Settings.OutlineColor);
+                cmd.Blit(m_tmpRT, m_cameraColorRT, Settings.CompositeMaterial);
+
+                context.ExecuteCommandBuffer(cmd);
+                CommandBufferPool.Release(cmd);
+            }
+
+            public override void FrameCleanup(CommandBuffer cmd)
+            {
+                if (m_cache == null)
+                {
+                    return;
+                }
+
+                cmd.ReleaseTemporaryRT(m_prepassId);
+                cmd.ReleaseTemporaryRT(m_blurredId);
+                cmd.ReleaseTemporaryRT(m_tmpTexId);
+            }
+        }
+
+        private RenderSelectionPass m_scriptablePass;
+        public override void Create()
+        {
+            m_scriptablePass = new RenderSelectionPass();
+            m_scriptablePass.Settings = m_settings;
+            m_scriptablePass.renderPassEvent = m_settings.RenderPassEvent;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            var src = renderer.cameraColorTarget;
+            m_scriptablePass.Setup(src);
+            renderer.EnqueuePass(m_scriptablePass);
+        }
+    }
+
+
+
+}
